5e023836727404181f5ac5d3f418e335f0484210
melancholiai <samit2310@gmail.com>
2022-03-24 16:59:10 +0200

    refactor: s3 client injection instead of config

--- src/retiler/tilesStorageProvider/s3.ts
+++ src/retiler/tilesStorageProvider/s3.ts
@@ -1,30 +1,23 @@
 /* eslint-disable @typescript-eslint/naming-convention */ // s3-client object commands arguments
-import { PutObjectCommand, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { inject, injectable } from 'tsyringe';
-import { S3_BUCKET, S3_CLIENT_CONFIG, TILE_PATH_LAYOUT } from '../../common/constants';
+import { S3_BUCKET, SERVICES, TILE_PATH_LAYOUT } from '../../common/constants';
 import { S3Error } from '../../common/errors';
-import { ShutdownHandler } from '../../common/shutdownHandler';
 import { TilesStorageProvider } from '../interfaces';
 import { TilePathLayout, tileToPathLayout } from '../tilesPath';
 import { TileWithBuffer } from '../types';
 
 @injectable()
 export class S3TilesStorage implements TilesStorageProvider {
-  private readonly s3Client: S3Client;
-
   public constructor(
-    @inject(ShutdownHandler) private readonly shutdownHandler: ShutdownHandler,
-    @inject(S3_CLIENT_CONFIG) clientConfig: S3ClientConfig,
+    @inject(SERVICES.S3) private readonly s3Client: S3Client,
+    @inject(S3_BUCKET) private readonly bucket: string,
     @inject(TILE_PATH_LAYOUT) private readonly tilePathLayout: TilePathLayout,
-
-    @inject(S3_BUCKET) private readonly bucket: string
-  ) {
-    this.s3Client = new S3Client(clientConfig);
-    this.shutdownHandler.addFunction(this.s3Client.destroy.bind(this));
-  }
+  ) {}
 
   public async storeTile(tile: TileWithBuffer): Promise<void> {
-    const key = tileToPathLayout(tile, this.tilePathLayout.tileLayout, `/${this.tilePathLayout.prefix}`, undefined, 'png');
+    console.log(this.bucket);
+    const key = tileToPathLayout(tile, this.tilePathLayout.tileLayout, `${this.tilePathLayout.prefix}`, undefined, 'png');
     const command = new PutObjectCommand({ Bucket: this.bucket, Key: key, Body: tile.buffer });
 
     try {
--- tests/unit/tilesStorageProvider/s3.spec.ts
+++ tests/unit/tilesStorageProvider/s3.spec.ts
@@ -1,31 +1,30 @@
 import { S3Client } from '@aws-sdk/client-s3';
 import { S3Error } from '../../../src/common/errors';
-import { ShutdownHandler } from '../../../src/common/shutdownHandler';
 import { TileLayout } from '../../../src/retiler/tilesPath';
 import { S3TilesStorage } from '../../../src/retiler/tilesStorageProvider/s3';
 
 jest.mock('@aws-sdk/client-s3');
 
-const mockedClient = S3Client.prototype as jest.Mocked<S3Client>;
-
 describe('S3TilesStorage', () => {
   describe('#storeTile', () => {
     let storage: S3TilesStorage;
+    let mockedS3Client: jest.Mocked<S3Client>;
+
     beforeEach(function () {
+      mockedS3Client = new S3Client({}) as jest.Mocked<S3Client>;
       storage = new S3TilesStorage(
-        { addFunction: jest.fn() } as unknown as ShutdownHandler,
-        {},
+        mockedS3Client,
+        'test-bucket',
         { prefix: 'test', tileLayout: TileLayout.ZXY, reverseY: false },
-        'test-bucket'
       );
     });
     afterEach(function () {
       jest.clearAllMocks();
     });
 
-    it('should resolve without an error if everything is ok', async function () {
+    it('should resolve without an error if client send resolved', async function () {
       const buffer = Buffer.from('test');
-      mockedClient.send.mockResolvedValue('' as never);
+      mockedS3Client.send.mockResolvedValue(undefined as never);
 
       const promise = storage.storeTile({
         buffer,
@@ -38,8 +37,8 @@ describe('S3TilesStorage', () => {
     });
 
     it('should throw an S3Error if the request failed', async function () {
-      const error = new Error('test');
-      mockedClient.send.mockRejectedValue(error as never);
+      const error = new Error('request failure error');
+      mockedS3Client.send.mockRejectedValue(error as never);
 
       const promise = storage.storeTile({
         buffer: Buffer.from('test'),
