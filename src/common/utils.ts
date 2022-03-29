00312755dfa2ca544ef30efe1e736923e470da3a
melancholiai <samit2310@gmail.com>
2022-03-29 14:01:43 +0300

    refactor: removed unnecessary util func

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,15 +1,5 @@
 import { Readable } from 'stream';
 
-export const streamToString = async (stream: Readable): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    stream.setEncoding('utf8');
-    let data = '';
-    stream.on('data', (chunk) => (data += chunk));
-    stream.on('error', reject);
-    stream.on('end', () => resolve(data));
-  });
-};
-
 export const bufferToStream = (buffer: Buffer): Readable => {
   const readableInstanceStream = new Readable({
     read(): void {
