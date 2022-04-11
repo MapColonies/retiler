8006021c03a274e5ed4a62f46380e0206a112af8
melancholiai <57397441+melancholiai@users.noreply.github.com>
2022-04-11 15:01:34 +0300

    refactor: exported liveness functionality from index (#9)
    * refactor: exported liveness functionality from index
    
    * refactor: according to pr

---
+++ src/common/liveness.ts
@@ -0,0 +1,39 @@
+import http from 'http';
+import { createTerminus } from '@godaddy/terminus';
+import { Logger } from '@map-colonies/js-logger';
+import { DependencyContainer } from 'tsyringe';
+import { IConfig, IServerConfig } from './interfaces';
+import { DEFAULT_SERVER_PORT, SERVICES } from './constants';
+import { ShutdownHandler } from './shutdownHandler';
+
+const stubHealthcheck = async (): Promise<void> => Promise.resolve();
+
+export const initLivenessProbe = (container: DependencyContainer): http.Server => {
+  const config = container.resolve<IConfig>(SERVICES.CONFIG);
+  const serverConfig = config.get<IServerConfig>('server');
+  const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
+
+  const server = http.createServer((request, response) => {
+    response.end(`running at http://localhost:${port}`);
+  });
+
+  const shutdownHandler = container.resolve(ShutdownHandler);
+
+  server.on('close', () => {
+    void shutdownHandler.shutdown();
+    server.unref();
+    process.exit();
+  });
+
+  createTerminus(server, {
+    healthChecks: { '/liveness': stubHealthcheck },
+    onSignal: shutdownHandler.shutdown.bind(shutdownHandler),
+  });
+
+  server.listen(port, () => {
+    const logger = container.resolve<Logger>(SERVICES.LOGGER);
+    logger.debug(`liveness on port ${port}`);
+  });
+
+  return server;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,29 @@
 /* eslint-disable import/first */
 // this import must be called before the first import of tsyringe
 import 'reflect-metadata';
-import http from 'http';
-import { createTerminus } from '@godaddy/terminus';
 import { Logger } from '@map-colonies/js-logger';
-import config from 'config';
 import { DependencyContainer } from 'tsyringe';
-import { DEFAULT_SERVER_PORT, JOB_QUEUE_PROVIDER, SERVICES } from './common/constants';
+import { JOB_QUEUE_PROVIDER, SERVICES } from './common/constants';
 import { ErrorWithExitCode } from './common/errors';
 import { ShutdownHandler } from './common/shutdownHandler';
 import { registerExternalValues } from './containerConfig';
 import { JobQueueProvider } from './retiler/interfaces';
 import { TileProcessor } from './retiler/tileProcessor';
+import { initLivenessProbe } from './common/liveness';
 
 let depContainer: DependencyContainer | undefined;
-
-interface IServerConfig {
-  port: string;
-}
-
-const serverConfig = config.get<IServerConfig>('server');
-const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
 
 void registerExternalValues()
   .then(async (container) => {
     depContainer = container;
-    const logger = container.resolve<Logger>(SERVICES.LOGGER);
 
-    const shutdownHandler = container.resolve(ShutdownHandler);
-
-    const stubHealthcheck = async (): Promise<void> => Promise.resolve();
-
-    const server = http.createServer((request, response) => {
-      response.end(`Hello World!\n\nThis is the Retiler server.\n\nThe server is running at http://localhost:${port}`);
-    });
-
-    server.on('close', () => {
-      void shutdownHandler.shutdown();
-      server.unref();
-      process.exit();
-    });
-
-    createTerminus(server, {
-      healthChecks: { '/liveness': stubHealthcheck },
-      onSignal: shutdownHandler.shutdown.bind(shutdownHandler),
-    });
-
-    server.listen(port, () => {
-      logger.info(`app started on port ${port}`);
-    });
+    const livenessProbe = initLivenessProbe(container);
 
     const processor = container.resolve(TileProcessor);
     const queueProv = container.resolve<JobQueueProvider>(JOB_QUEUE_PROVIDER);
-
     await queueProv.consumeQueue(processor.processTile.bind(processor));
 
-    server.close();
+    livenessProbe.close();
   })
   .catch(async (error: ErrorWithExitCode) => {
     const errorLogger = depContainer?.isRegistered(SERVICES.LOGGER) === true ? depContainer.resolve<Logger>(SERVICES.LOGGER).error : console.error;
