c3b09a0fdee4b00c8864525ae1d6d280e4354bf3
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2022-08-28 15:55:21 +0300

    style: prettier

--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,12 @@ void registerExternalValues()
     const shutdownHandler = container.resolve(ShutdownHandler);
     await shutdownHandler.shutdown();
   })
-  .catch(async (error) => {
+  .catch(async (error: Error) => {
     const errorLogger =
       depContainer?.isRegistered(SERVICES.LOGGER) == true
         ? depContainer.resolve<Logger>(SERVICES.LOGGER).error.bind(depContainer.resolve<Logger>(SERVICES.LOGGER))
         : console.error;
-    errorLogger({msg: 'an unexpected error occurred', err: error});
+    errorLogger({ msg: 'an unexpected error occurred', err: error });
 
     if (depContainer?.isRegistered(ShutdownHandler) === true) {
       const shutdownHandler = depContainer.resolve(ShutdownHandler);
