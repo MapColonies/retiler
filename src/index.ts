3588ef78ad39738e7cf45a04a0494bd0a8e156bb
Ofer <12687466+CptSchnitz@users.noreply.github.com>
2022-08-28 15:25:20 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,11 @@ void registerExternalValues()
     await shutdownHandler.shutdown();
   })
   .catch(async (error) => {
-    const errorLogger = depContainer?.isRegistered(SERVICES.LOGGER) === true ? depContainer.resolve<Logger>(SERVICES.LOGGER).error : console.error;
-    errorLogger('an unexpected error occurred', error);
+    const errorLogger =
+      depContainer?.isRegistered(SERVICES.LOGGER) == true
+        ? depContainer.resolve<Logger>(SERVICES.LOGGER).error.bind(depContainer.resolve<Logger>(SERVICES.LOGGER))
+        : console.error;
+    errorLogger({msg: 'an unexpected error occurred', err: error});
 
     if (depContainer?.isRegistered(ShutdownHandler) === true) {
       const shutdownHandler = depContainer.resolve(ShutdownHandler);
