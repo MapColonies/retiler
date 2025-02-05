fc9778185fdae93d7974e0282773a75cd6cba911
Niv Greenstein <greenstein9@gmail.com>
2025-02-05 16:36:03 +0200

    chore: added console.log

--- tests/configurations/integration/jest.globalSetup.ts
+++ tests/configurations/integration/jest.globalSetup.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */ // s3-client object commands arguments
 import { S3Client, CreateBucketCommand, HeadBucketCommand } from '@aws-sdk/client-s3';
+import { serializeError } from '@common.js/serialize-error';
 import config from 'config';
 import { S3StorageProviderConfig, StorageProviderConfig } from '../../../src/retiler/tilesStorageProvider/interfaces';
 
@@ -14,11 +15,13 @@ export default async (): Promise<void> => {
     }
 
     const { kind, bucketName, ...clientConfig } = provider as S3StorageProviderConfig;
+    console.log(clientConfig);
     const s3Client = new S3Client(clientConfig);
 
     try {
       await s3Client.send(new HeadBucketCommand({ Bucket: bucketName }));
     } catch (error) {
+      console.log(serializeError(error));
       const s3Error = error as Error;
       if (s3Error.name !== 'NotFound') {
         throw s3Error;
