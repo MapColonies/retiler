a1fe743474c054bf7255c331bea89659305a1dc9
Niv Greenstein <greenstein9@gmail.com>
2025-04-10 11:02:33 +0300

    chore: removed unused types

--- src/retiler/types.ts
+++ src/retiler/types.ts
@@ -11,6 +11,4 @@ export type TileWithMetadata = Required<Tile> & Partial<TileMetadata>;
 
 export type TileWithBuffer = TileWithMetadata & { buffer: Buffer };
 
-export type MapProviderType = 'wms' | 'arcgis';
-
 export type WmsVersion = '1.1.1' | '1.3.0';
